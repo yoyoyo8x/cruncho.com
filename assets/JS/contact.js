933293e42db9d25fb292984583d7f95ed5beae31
yoyoyo8x <ductrung8697@gmail.com>
2022-12-02 00:44:19 +0700

    up

--- assets/JS/contact.js
+++ assets/JS/contact.js
@@ -1,15 +1,31 @@
 var firstRequired = document.getElementById("1st-required");
 var secondRequired = document.getElementById("2nd-required");
 var emailRequired = document.getElementById("email-required");
+var emailInvalid = document.getElementById("email-invalid");
+var subAjax = document.getElementById("submit-ajax");
 
-document.myForm.onsubmit = (e) => {
-  e.preventDefault();
-};
+document.myForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  subAjax.style.visibility = "visible";
+  setTimeout(function () {
+    subAjax.style.visibility = "hidden";
+  }, 1500);
+});
+
 function store() {
   // event.preventDefault();
   var firstName = document.getElementById("first-n").value;
   var secondName = document.getElementById("second-n").value;
   var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  validEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+  const info = {
+    firstName: firstName,
+    secondName: secondName,
+    email: email,
+    message: message,
+  };
 
   if (firstName === "" || firstName === null) {
     firstRequired.style.display = "block";
@@ -25,8 +41,15 @@ function store() {
 
   if (email === "" || email === null) {
     emailRequired.style.display = "block";
-  } else {
+    emailInvalid.style.display = "none";
+  }
+  if (email !== "" && validEmail.test(email) === false) {
+    emailInvalid.style.display = "block";
     emailRequired.style.display = "none";
+  }
+  if (email !== "" && validEmail.test(email) === true) {
+    emailRequired.style.display = "none";
+    emailInvalid.style.display = "none";
   }
 
   if (
@@ -35,15 +58,11 @@ function store() {
     secondName === "" ||
     secondName === null ||
     email === "" ||
-    email === null
+    email === null ||
+    validEmail.test(email) === false
   ) {
     return false;
   }
-  const info = {
-    firstName: firstName,
-    secondName: secondName,
-    email: email,
-  };
 
   localStorage.setItem("userInfo", JSON.stringify(info));
   const userInfo = localStorage.getItem("userInfo");
