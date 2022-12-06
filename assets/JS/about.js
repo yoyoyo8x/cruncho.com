2701d1abd0969251b967a1b1aa9765b03e171c77
yoyoyo8x <ductrung8697@gmail.com>
2022-12-06 20:09:55 +0700

    up

--- assets/JS/about.js
+++ assets/JS/about.js
@@ -1,21 +1,22 @@
-
-
 // Slideshow2
 let Move = 0;
 Persay();
 //dot
 function Change(n) {
-  if (n > slides.length) {n = 1}
-      else if(n < 1) {n = slides.length}
+  if (n > slides.length) {
+    n = 1;
+  } else if (n < 1) {
+    n = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-     slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  
-  slides[n-1].style.display = "flex";  
-  dots[n-1].className += " active";
+
+  slides[n - 1].style.display = "flex";
+  dots[n - 1].className += " active";
 }
 //auto
 function Persay() {
@@ -23,18 +24,19 @@ function Persay() {
   slides = document.getElementsByClassName("sl_show per");
   dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   Move++;
-  if (Move > slides.length) {Move = 1}    
+  if (Move > slides.length) {
+    Move = 1;
+  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[Move-1].style.display = "flex";  
-  dots[Move-1].className += " active";
+  slides[Move - 1].style.display = "flex";
+  dots[Move - 1].className += " active";
   setTimeout(Persay, 3000); // Change image every 3 seconds
 }
-
 
 // AUTOSCROLL
 var autoBtn = document.getElementById("auto-btn");
@@ -53,7 +55,6 @@ function scrollFunction() {
 function autoScroll() {
   document.documentElement.scroll({ top: 0, behavior: "smooth" });
 }
-
 
 // MENUBAR
 var navBtn = document.getElementById("nav-mobile-btn");
@@ -80,62 +81,59 @@ function hideMenu() {
   }, 500);
 }
 
-
 // LOADING
-var loadAjax = document.getElementById("loading")
-window.addEventListener("load", function(){
+var loadAjax = document.getElementById("loading");
+window.addEventListener("load", function () {
   // loadAjax.classList.add("blur");
-  setTimeout(
-      function (){
-          loadAjax.style.display = "none";
-      },
-     300
-  )
+  setTimeout(function () {
+    loadAjax.classList.add("fade-in");
+    loadAjax.style.display = "none";
+  }, 500);
 });
 
-
-
 // ! AnimationNumber
-function animateNumber(finalNumber, duration = 3000, startNumber = 0, callback) {
-  let currentNumber = startNumber
-  const interval = window.setInterval(updateNumber, 17)
+function animateNumber(
+  finalNumber,
+  duration = 3000,
+  startNumber = 0,
+  callback
+) {
+  let currentNumber = startNumber;
+  const interval = window.setInterval(updateNumber, 17);
   function updateNumber() {
     if (currentNumber >= finalNumber) {
-      clearInterval(interval)
+      clearInterval(interval);
     } else {
-      let inc = Math.ceil(finalNumber/ (duration / 17))
+      let inc = Math.ceil(finalNumber / (duration / 17));
       if (currentNumber + inc > finalNumber) {
-        currentNumber = finalNumber
-        clearInterval(interval)
+        currentNumber = finalNumber;
+        clearInterval(interval);
       } else {
-        currentNumber += inc
+        currentNumber += inc;
       }
-      callback(currentNumber)
-    }  
+      callback(currentNumber);
+    }
   }
 }
 
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   animateNumber(5337, 3000, 0, function (number) {
-    const formattedNumber = number.toLocaleString()
-    document.getElementById('happyclient-count').innerText = formattedNumber
-  })
-  
+    const formattedNumber = number.toLocaleString();
+    document.getElementById("happyclient-count").innerText = formattedNumber;
+  });
+
   animateNumber(1230, 3000, 0, function (number) {
-    const formattedNumber = number.toLocaleString()
-    document.getElementById('amazingdish-count').innerText = formattedNumber
-  })
-  
+    const formattedNumber = number.toLocaleString();
+    document.getElementById("amazingdish-count").innerText = formattedNumber;
+  });
+
   animateNumber(251, 3500, 0, function (number) {
-    const formattedNumber = number.toLocaleString()
-    document.getElementById('satisfiedstaff-count').innerText = formattedNumber
-  })
+    const formattedNumber = number.toLocaleString();
+    document.getElementById("satisfiedstaff-count").innerText = formattedNumber;
+  });
 
   animateNumber(9437, 3000, 0, function (number) {
-    const formattedNumber = number.toLocaleString()
-    document.getElementById('workhour-count').innerText = formattedNumber
-  })
-
-})
-
+    const formattedNumber = number.toLocaleString();
+    document.getElementById("workhour-count").innerText = formattedNumber;
+  });
+});
